d7c74fbd3134764e46099e5c94cf500d55d0830b
unknown <mahmud@artificial-soft.com>
2019-07-03 12:21:00 +0600

    Application scope added -dagger

---
+++ app/src/main/java/com/oneteam/advanceandroid/base/ApplicationComponent.java
@@ -0,0 +1,11 @@
+package com.oneteam.advanceandroid.base;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+@Singleton
+@Component(modules = {ApplicationModule.class})
+public interface ApplicationComponent {
+
+}
